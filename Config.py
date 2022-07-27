6fa2aef008c5c11a0eba3bb0d19afdc941c7c28a
Lependosa <106450408+Lependosa@users.noreply.github.com>
2022-07-27 18:03:09 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("APP_ID", "19584439"))
+    API_HASH = os.environ.get("API_HASH", "6d15c23ed0237793e61795a18ea9ecc8")
+    BOT_TOKEN = os.environ.get("5498541898:AAEOAzHWnRe8PF09xxjJSXKe6mza8lPPd4I", "")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Zaid2_Robot")
-    SUPPORT = os.environ.get("SUPPORT", "TheSupportChat")
-    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Nanamusic")
+    SUPPORT = os.environ.get("SUPPORT", "Cari_Temen_Pacar_Online")
+    CHANNEL = os.environ.get("CHANNEL", "Ruangkatastory")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/dec375214379eeb6e2980.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/a97bee892ad48b4f34e70.jpg")
